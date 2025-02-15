25c2ec97580038334e24b407a92dd42f3f6ec3f5
Bhanu prakash <107290069+Eee255@users.noreply.github.com>
2025-02-15 22:58:30 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectionDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://bhanu171:kZP3YurHFp8LtFS6@bhanu.rul9b.mongodb.net/devtinder"
+        "mongodb+srv://bhanu171:kZP3YurHFp8LtFS6@bhanu.rul9b.mongodb.net/devTinder"
     );
 }
 
